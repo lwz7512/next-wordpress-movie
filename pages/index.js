bc8eced63c456f056a35d1c7e4099363eb32e727
wenzhi.li <wenzhi.li@verbinteractive.com>
2021-04-23 09:14:19 -0300

    .gitignore is now working

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,27 @@
 import Head from 'next/head'
 import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPostsForHome } from '../lib/api'
-import { CMS_NAME } from '../lib/constants'
 
-export default function Index({ allPosts: { edges }, preview }) {
-  const heroPost = edges[0]?.node
-  const morePosts = edges.slice(1)
+export default function Index() {
+  
 
   return (
     <>
       <Layout preview={preview}>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>Next.js Site with WPACF</title>
         </Head>
         <Container>
           <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.featuredImage?.node}
-              date={heroPost.date}
-              author={heroPost.author?.node}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+          {/* TODO: Movie cards */}
         </Container>
       </Layout>
     </>
   )
 }
 
-export async function getStaticProps({ preview = false }) {
-  const allPosts = await getAllPostsForHome(preview)
-  return {
-    props: { allPosts, preview },
-  }
-}
+// export async function getStaticProps({ preview = false }) {
+//   const allPosts = await getAllPostsForHome(preview)
+//   return {
+//     props: { allPosts, preview },
+//   }
+// }
